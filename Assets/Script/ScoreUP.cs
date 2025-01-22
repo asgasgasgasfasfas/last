fa3adfce220a15a397d444cd39b03ce61edd0f8b
DESKTOP-CE6GC3O\jjg19 <jjg1945@hanmail.net>
2025-01-22 17:46:54 +0900

    아이템 작업중 생성 확율

--- Assets/Script/ScoreUP.cs
+++ Assets/Script/ScoreUP.cs
@@ -4,12 +4,23 @@ using UnityEngine;
 
 public class ScoreUP : MonoBehaviour
 {
-    public PlayerControl playerControl;
+    public PlayerControl playerController;
+    public int item = 1;
 
     // 파이프 통과시 점수
+    private void Awake()
+    {
+        //tip 현재 코드에서는 한번만 호출하기 떄문에 ondie() 함수에서 바로 호출해도 되지만
+        //오브젝트 풀링을 이욧ㅇ해 오브젝트를 재사용할 경우에는 최소 1번만  find  를 이용해
+        //   playerController 의 정보를 저장해두고 사용하는 것이 연산에 효율적이다
+        playerController = GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerControl>();
+        //부닥친 적의 정보를 얻어오는 코드
+          itmePrb = GetComponent<Item>();
+
+    }
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        playerControl.Score++;
-        Debug.Log(playerControl.Score);
+        playerController.Score = 1 * item;
+        Debug.Log(playerController.Score);
     }
 }
